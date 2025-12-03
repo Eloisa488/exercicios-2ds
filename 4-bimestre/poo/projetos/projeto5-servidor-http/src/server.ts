026bc6c491aa31ccd352c9d63cff81e0d93ec25d
Eloisa488 <rodrigueseloisa488@gmail.com>
2025-12-03 22:22:50 +0000

    CREATE: projeto 5 feito

---
+++ 4-bimestre/poo/projetos/projeto5-servidor-http/src/server.ts
@@ -0,0 +1,18 @@
+import http from 'node:http'
+import 'dotenv/config'
+
+const nome = 'John'
+const idade = 34
+const port = process.env.PORT
+
+const server = http.createServer((req, res) => {
+    res.end(`Servidor rodando para ${nome}, idade ${idade}!`)
+})
+
+server.listen(port, () => {
+    console.log(`Servidor funcionando em http://localhost:${port}`)
+}).close(() => {
+  console.log("Servidor encerrado.")
+})
+
+export default server
